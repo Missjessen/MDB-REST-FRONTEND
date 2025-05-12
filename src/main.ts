d827601b80f0fadf0b5b892fc709821a18434145
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2025-05-12 18:02:28 +0200

    axios axios defaults

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import App             from './App.vue'
 import router          from './router'
 import './assets/tailwind.css' 
 import { useAuthStore } from '@/modules/auth/store' 
+import axios           from 'axios'
 
 
 const app   = createApp(App)
 const pinia = createPinia()
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE;
 
 app.use(pinia)   // <— først Pinia
 app.use(router)  // <— så router (der kører beforeEach)
