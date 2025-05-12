8e9e08c5ddeed0460e6a9bb3f22867911175e40b
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2025-05-12 13:14:14 +0200

    ændring af url og env

--- src/modules/auth/api.ts
+++ src/modules/auth/api.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 export const authApi = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE}/auth`,
-  withCredentials: true,
-  headers: { 'Content-Type': 'application/json' }
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true
 })
+
